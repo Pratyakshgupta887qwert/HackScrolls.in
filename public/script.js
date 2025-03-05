da0df37b443dbca41a7043921174ed0ab0a430ed
SumitSharma2124 <sumitzsharma2124@gmail.com>
2025-03-05 22:00:27 +0530

    darkmode update

--- public/script.js
+++ public/script.js
@@ -13,3 +13,25 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("login-form").style.display = "block";
     document.getElementById("signup-form").style.display = "none";
 });
+
+
+const toggleButton = document.getElementById("darkModeToggle");
+
+window.onload = () => {
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark-mode");
+        toggleButton.src = "assets/darkmode.png";
+    }
+};
+
+toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+        toggleButton.src = "assets/darkmode.png";
+    } else {
+        localStorage.setItem("theme", "light");
+        toggleButton.src = "assets/lightmode.png";
+    }
+});
